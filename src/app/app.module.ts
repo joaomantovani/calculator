6b5a5ac8f63efd7a08eb86ec2621361ae714e867
João Pedro Mantovani <jpm.mantovani45@gmail.com>
2019-01-21 22:36:46 -0200

    Issue #2
    Integrate with some CSS library
    
    Add the Ngx Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 
 @NgModule({
   declarations: [
